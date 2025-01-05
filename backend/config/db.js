96e9ca93f0b52890d3400c02f92c10d6ff1a7dd1
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-01-05 11:21:54 +0700

    clear func

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,1 @@
 // DATABASE_URL = "ini adalah database url"
-
-const a = a => (a * a);
-console.log(a(2,3))
-const b = b => ( b * b);
-console.log(2,4)
-
-const hai = (name) => {
-  const hais= `haii ${name}`;
-  return hais;
-}
-console.log(hai("fadils"));
-
-function greet(name){
-  const greets = `hallo ${name}`
-  return greets;
-}
-console.log(greet("lizard"));
